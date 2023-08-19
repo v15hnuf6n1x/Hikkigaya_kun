d9e2a14ab33a3d9fe000122c7333e83287c1a054
VISHNU <Vishnuvishva2434@gmail.com>
2023-08-20 01:57:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/v15hnuf6n1x/ /REPONAME
+  git clone https://github.com/v15hnuf6n1x/Hikkigaya_kun
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /REPONAME
+  git clone $UPSTREAM_REPO /Hikkigaya_kun
 fi
-cd /REPONAMEPiroAutoFilterBot
+cd /Hikkigaya_kun
 pip3 install -U -r requirements.txt
-echo "Starting BOTNAME...."
+echo "Starting Hikkigaya_kun..."
 python3 bot.py
